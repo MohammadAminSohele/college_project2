3dcc93977d2162f88105217a97bbf1ee1ca93800
Mohammad Amin Sohaili <mohammadaminsohele@gmail.com>
2023-12-25 13:59:49 +0330

    login view

--- account/views.py
+++ account/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login
+
+
+from .forms import login_form
 
 # Create your views here.
 
 """ login view """
 def login_view(request):
-    return render(request,'account/login_page.html',{})
+    form = login_form(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+    context={
+        'form':form
+    }
+    return render(request,'account/login_page.html',context)
