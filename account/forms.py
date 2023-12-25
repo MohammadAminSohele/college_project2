fca5c7e9a0f1e3bac2deb5efea852286904a6f5c
Mohammad Amin Sohaili <mohammadaminsohele@gmail.com>
2023-12-25 14:10:40 +0330

    register view

--- account/forms.py
+++ account/forms.py
@@ -10,3 +10,30 @@ class login_form(forms.Form):
         widget=forms.TextInput(),
         label='پسورد'
     )
+
+""" register form """
+class register_form(forms.Form):
+    Username=forms.CharField(
+        widget=forms.TextInput(),
+        label='نام کاربری'
+    )
+    email=forms.EmailField(
+        widget=forms.EmailInput(),
+        label='ایمیل'
+    )
+    first_name = forms.CharField(
+        widget=forms.TextInput(),
+        label='نام'
+    )
+    last_name = forms.CharField(
+        widget=forms.TextInput(),
+        label='نام خانوداگی'
+    )
+    Password=forms.CharField(
+        widget=forms.PasswordInput(),
+        label='پسورد'
+    )
+    Re_Password=forms.CharField(
+        widget=forms.PasswordInput(),
+        label='تکرار پسورد'
+    )
--- account/views.py
+++ account/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login
+from django.contrib.auth.models import User
 
 
-from .forms import login_form
+from .forms import login_form,register_form
 
 # Create your views here.
 
@@ -20,3 +21,21 @@ def login_view(request):
         'form':form
     }
     return render(request,'account/login_page.html',context)
+
+""" register view """
+def register_view(request):
+    form = register_form(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get('Username')
+        email = form.cleaned_data.get('email')
+        first_name = form.cleaned_data.get('first_name')
+        last_name = form.cleaned_data.get('last_name')
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('Password')
+        User.objects.create_user(
+            username=username, email=email, password=password, first_name=first_name, last_name=last_name)
+        return redirect('/account/login')
+    context={
+        'form':form
+    }
+    return render(request,'account/register_page.html',context)
