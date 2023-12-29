45c82f53f39682647a3c07490ed75fb525e66b46
MohammadAminSohaili <mohammadaminsohele@gmail.com>
2023-12-29 10:03:25 +0330

    register student info

--- college/views.py
+++ college/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import StudentForm
 
 # Create your views here.
+
+def register_student(request):
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = StudentForm()
+    return render(request, 'college/register_student.html', {'form': form})
